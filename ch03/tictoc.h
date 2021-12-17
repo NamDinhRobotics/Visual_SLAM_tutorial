e16bd80e24d8af47af42f13e111b98ec7855e817
Dinh Van Nam <30413547+NamDinhRobotics@users.noreply.github.com>
2021-12-17 18:20:12 +0900

    up

---
+++ ch03/tictoc.h
@@ -0,0 +1,30 @@
+//
+// Created by dinhnambkhn on 21. 12. 17..
+//
+#include <chrono>
+
+class TicToc {
+public:
+    TicToc() {
+        tic();
+    }
+
+    void tic() {
+        start = std::chrono::system_clock::now();
+    }
+
+    auto toc_ms() {
+        end = std::chrono::system_clock::now();
+        return std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count();
+    }
+
+    auto toc_us() {
+        end = std::chrono::system_clock::now();
+        return std::chrono::duration_cast<std::chrono::microseconds>(end - start).count();
+    }
+
+
+    private:
+        std::chrono::time_point<std::chrono::system_clock> start, end;
+
+};
